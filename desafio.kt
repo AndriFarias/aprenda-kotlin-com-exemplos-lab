ae662ff6f1fe199958ef419c53bcea37d80614ce
AndriFarias <99991197+AndriFarias@users.noreply.github.com>
2023-04-18 13:05:16 -0400

    resolvendo desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,39 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val idade:Int)
 
-class Usuario
+data class ConteudoEducacional(val nome: String,val nivel:Nivel, val duracao: Int = 60)
+fun conteudoEduca(): List<ConteudoEducacional>{
+    return listOf(
+    ConteudoEducacional("Aprendendo variaveis com Kotlin", Nivel.BASICO),
+    ConteudoEducacional("Aprendendo funcões com Kotlin", Nivel.INTERMEDIARIO, duracao = 120)
+    )
+}   
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>){
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        val aluno = usuario.nome.toString()
+        println("O aluno $aluno foi matriculado com sucesso!")
+    }
+    fun listarInscritos():Unit{
+        println(inscritos)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val aluno1 = Usuario("Lucia",20)
+    val aluno2 = Usuario("Ana",25)
+    val formacao1= Formacao("Kotlin",conteudoEduca())
+    formacao1.matricular(aluno1)
+    formacao1.matricular(aluno2)
+    formacao1.listarInscritos()
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
 }
